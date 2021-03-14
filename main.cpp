1bae255bb82661fc8e098baef12e3c3f0c884b3b
yeip1 <juan.castillog@sansano.usm.cl>
2021-03-14 15:57:25 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -161,15 +161,15 @@ int main(){
     // char file_path1[] = "./IntervalSets/infectious/infectiousA_415912.txt";
     // char file_path2[] = "./IntervalSets/infectious/infectiousB_415912.txt";
 
-    // char file_path1[] = "./IntervalSets/infectious/infectiousA_200000.txt";
-    // char file_path2[] = "./IntervalSets/infectious/infectiousB_200000.txt";
+    char file_path1[] = "./IntervalSets/infectious/infectiousA_200000.txt";
+    char file_path2[] = "./IntervalSets/infectious/infectiousB_200000.txt";
 
 
     // char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexA10_1000000.txt";
     // char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexB10_1000000.txt";
 
-    char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexA10_1000000.txt";
-    char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexB100_1000000.txt";
+    // char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexA10_1000000.txt";
+    // char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexB100_1000000.txt";
 
     // char file_path1[] = "./../../../data/intervals/atusac/atusacA_100.txt";
     // char file_path2[] = "./../../../data/intervals/atusac/atusacB_100.txt";
