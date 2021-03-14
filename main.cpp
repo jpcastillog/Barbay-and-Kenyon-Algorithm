0910ddaf9a93abac363bcc58c093857fd5c43903
yeip1 <juan.castillog@sansano.usm.cl>
2021-03-14 18:11:00 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -190,8 +190,8 @@ int main(){
     // char file_path2[] = "./../../../data/intervals/atusac/atusacB_1000000.txt";
 
 
-    char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexADefault_1000000.txt";
-    char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexBDefault_1000000.txt";
+    char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexADefault_100000.txt";
+    char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexBDefault_100000.txt";
 
 
     // Se cargan los conjuntos de intervalos
