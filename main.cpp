8f8a03979098adff4fda30877a199f67c745a912
yeip1 <juan.castillog@sansano.usm.cl>
2021-03-26 01:29:35 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -180,8 +180,8 @@ int main(){
     // char file_path1[] = "./../../../data/intervals/atusac/atusacA_10000.txt";
     // char file_path2[] = "./../../../data/intervals/atusac/atusacB_10000.txt";
 
-    // char file_path1[] = "./../../../data/intervals/atusac/atusacA_100000.txt";
-    // char file_path2[] = "./../../../data/intervals/atusac/atusacB_100000.txt";
+    char file_path1[] = "./../../../data/intervals/atusac/atusacA_100000.txt";
+    char file_path2[] = "./../../../data/intervals/atusac/atusacB_100000.txt";
 
     // char file_path1[] = "./../../../data/intervals/atusac/atusacA_500000.txt";
     // char file_path2[] = "./../../../data/intervals/atusac/atusacB_500000.txt";
@@ -190,8 +190,8 @@ int main(){
     // char file_path2[] = "./../../../data/intervals/atusac/atusacB_1000000.txt";
 
 
-    char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexADefault_1000000.txt";
-    char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexBDefault_1000000.txt";
+    // char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexADefault_1000000.txt";
+    // char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexBDefault_1000000.txt";
 
 
     // Se cargan los conjuntos de intervalos
