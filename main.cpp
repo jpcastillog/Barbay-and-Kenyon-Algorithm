36f8f9fc87d27993b38e2ad856bea333deda1c0c
yeip1 <juan.castillog@sansano.usm.cl>
2021-03-28 03:43:35 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -177,8 +177,8 @@ int main(){
     // char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexA10_1000000.txt";
     // char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexB10_1000000.txt";
 
-    char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexA100_1000000.txt";
-    char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexB100_1000000.txt";
+    char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexB100_500000.txt";
+    char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexA100_500000.txt";
 
     // char file_path1[] = "./../../../data/intervals/invertedIndex/invertedIndexA10_1000000.txt";
     // char file_path2[] = "./../../../data/intervals/invertedIndex/invertedIndexB100_1000000.txt";
